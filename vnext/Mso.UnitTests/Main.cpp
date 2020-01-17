4d6d625b4ce75a88b72688d4dfb126dd2c8ee44e
Vladimir Morozov <vmorozov@microsoft.com>
2020-01-17 06:20:41 -0800

    Added modified Mso library with Mso::Future

--- vnext/Mso.UnitTests/Main.cpp
+++ vnext/Mso.UnitTests/Main.cpp
@@ -1,9 +1,10 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
-#include <gtest/gtest.h>
+#include "motifCpp/gTestAdapter.h"
 
 int main(int argc, char **argv) {
+  Mso::UnitTests::GTest::RegisterUnitTests();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
