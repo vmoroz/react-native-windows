bc261694af2d95f0297b29e1b6c1c67fbb8f04aa
Vladimir Morozov <vmorozov@microsoft.com>
2020-01-20 00:31:40 -0800

    Initial attempt - does not compile.

---
+++ vnext/SharedPch/ProjectPch.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This is a PCH file used by projects that do not use Shared PCH
+
+#include "pch.h"
---
+++ vnext/SharedPch/pch.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "SharedPch.h"
+#include "pch.h"
+
+// Pragma hdrstop specifies name of the project's PCH file to be created.
+// PrecompiledHeaderOutputFile value is provided in the Win8CommonProperties.props file.
+#pragma hdrstop(PrecompiledHeaderOutputFile)
