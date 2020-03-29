af2881614935cb2c9b6e5760d3282defda04543b
Vladimir Morozov <vmorozov@microsoft.com>
2020-03-28 18:17:32 -0700

    More project updates

--- vnext/Desktop/module.g.cpp
+++ vnext/Desktop/module.g.cpp
@@ -7,7 +7,7 @@ void* winrt_make_facebook_react_MemoryTracker();
 void* winrt_make_facebook_react_NativeLogEventSource();
 void* winrt_make_facebook_react_NativeTraceEventSource();
 
-bool __stdcall Desktop_can_unload_now() noexcept
+bool __stdcall winrt_can_unload_now() noexcept
 {
     if (winrt::get_module_lock())
     {
@@ -18,7 +18,7 @@ bool __stdcall Desktop_can_unload_now() noexcept
     return true;
 }
 
-void* __stdcall Desktop_get_activation_factory([[maybe_unused]] std::wstring_view const& name)
+void* __stdcall winrt_get_activation_factory([[maybe_unused]] std::wstring_view const& name)
 {
     auto requal = [](std::wstring_view const& left, std::wstring_view const& right) noexcept
     {
@@ -42,3 +42,33 @@ void* __stdcall Desktop_get_activation_factory([[maybe_unused]] std::wstring_vie
 
     return nullptr;
 }
+
+int32_t __stdcall WINRT_CanUnloadNow() noexcept
+{
+#ifdef _WRL_MODULE_H_
+    if (!::Microsoft::WRL::Module<::Microsoft::WRL::InProc>::GetModule().Terminate())
+    {
+        return 1;
+    }
+#endif
+
+    return winrt_can_unload_now() ? 0 : 1;
+}
+
+int32_t __stdcall WINRT_GetActivationFactory(void* classId, void** factory) noexcept try
+{
+    std::wstring_view const name{ *reinterpret_cast<winrt::hstring*>(&classId) };
+    *factory = winrt_get_activation_factory(name);
+
+    if (*factory)
+    {
+        return 0;
+    }
+
+#ifdef _WRL_MODULE_H_
+    return ::Microsoft::WRL::Module<::Microsoft::WRL::InProc>::GetModule().GetActivationFactory(static_cast<HSTRING>(classId), reinterpret_cast<::IActivationFactory**>(factory));
+#else
+    return winrt::hresult_class_not_available(name).to_abi();
+#endif
+}
+catch (...) { return winrt::to_hresult(); }
