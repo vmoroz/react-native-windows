bd39b12a91aa3029b9e0abda37644f3a7e2a5db6
Vladimir Morozov <vmorozov@microsoft.com>
2020-05-15 20:50:44 -0700

    Implemented code for the ReactNotificationService

--- vnext/Microsoft.ReactNative/IReactNotificationService.cpp
+++ vnext/Microsoft.ReactNative/IReactNotificationService.cpp
@@ -6,7 +6,206 @@
 #include "ReactNotificationServiceHelper.g.cpp"
 
 namespace winrt::Microsoft::ReactNative::implementation {
-Microsoft::ReactNative::IReactNotificationService ReactNotificationServiceHelper::CreateNotificationService() {
-  throw hresult_not_implemented();
+
+//=============================================================================
+// ReactNotificationSubscription implementation
+//=============================================================================
+
+ReactNotificationSubscription::ReactNotificationSubscription(
+    weak_ref<ReactNotificationService> &&notificationService,
+    IReactDispatcher const &dispatcher,
+    IReactPropertyName const &notificationName,
+    ReactNotificationHandler const &handler) noexcept
+    : m_notificationService{std::move(notificationService)},
+      m_dispatcher{dispatcher},
+      m_notificationName{notificationName},
+      m_handler{handler} {}
+
+ReactNotificationSubscription::~ReactNotificationSubscription() noexcept {
+  Unsubscribe();
 }
+
+IReactDispatcher ReactNotificationSubscription::Dispatcher() const noexcept {
+  return m_dispatcher;
+}
+
+IReactPropertyName ReactNotificationSubscription::NotificationName() const noexcept {
+  return m_notificationName;
+}
+
+bool ReactNotificationSubscription::IsSubscribed() const noexcept {
+  return m_isSubscribed;
+}
+
+void ReactNotificationSubscription::Unsubscribe() noexcept {
+  if (m_isSubscribed.exchange(false)) {
+    if (auto notificationService = m_notificationService.get()) {
+      notificationService->Unsubscribe(*this);
+    }
+  }
+}
+
+void ReactNotificationSubscription::CallHandler(
+    IInspectable const &sender,
+    IReactNotificationData const &notificationData) noexcept {
+  if (IsSubscribed()) {
+    if (m_dispatcher) {
+      m_dispatcher.Post([thisPtr = get_strong(), sender, notificationData]() noexcept {
+        if (thisPtr->IsSubscribed()) {
+          thisPtr->m_handler(sender, notificationData);
+        }
+      });
+    } else {
+      m_handler(sender, notificationData);
+    }
+  }
+}
+
+//=============================================================================
+// ReactNotificationService implementation
+//=============================================================================
+
+ReactNotificationService::ReactNotificationService() = default;
+
+ReactNotificationService::ReactNotificationService(IReactNotificationService const parentNotificationService) noexcept
+    : m_parentNotificationService{parentNotificationService} {}
+
+ReactNotificationService::~ReactNotificationService() = default;
+
+void ReactNotificationService::ModifySubscriptions(
+    IReactPropertyName const &notificationName,
+    Mso::FunctorRef<SubscriptionSnapshot(SubscriptionSnapshot const &)> const &modifySnapshot) {
+  // Get the current snapshot under the lock
+  SubscriptionSnapshotPtr currentSnapshotPtr;
+  {
+    std::scoped_lock readLock{m_mutex};
+    auto it = m_subscriptions.find(notificationName);
+    if (it != m_subscriptions.end()) {
+      currentSnapshotPtr = it->second;
+    }
+  }
+
+  // Modify the snapshot under the loop until we succeed.
+  for (;;) {
+    // Create new snapshot outside of lock.
+    auto newSnapshot = modifySnapshot(currentSnapshotPtr ? *currentSnapshotPtr : SubscriptionSnapshot());
+
+    // Try to set the new snapshot under the lock
+    SubscriptionSnapshotPtr snapshotPtr;
+    std::scoped_lock readLock{m_mutex};
+    auto it = m_subscriptions.find(notificationName);
+    if (it != m_subscriptions.end()) {
+      snapshotPtr = it->second;
+    }
+
+    if (currentSnapshotPtr != snapshotPtr) {
+      // Some other thread already changed the snapshot for our subscription. Do it again.
+      currentSnapshotPtr = std::move(snapshotPtr);
+      continue;
+    }
+
+    if (newSnapshot.empty()) {
+      if (it != m_subscriptions.end()) {
+        m_subscriptions.erase(it);
+      }
+    } else {
+      auto newSnapshotPtr = Mso::Make_RefCounted<SubscriptionSnapshot>(std::move(newSnapshot));
+      if (it != m_subscriptions.end()) {
+        it->second = std::move(newSnapshotPtr);
+      } else {
+        m_subscriptions.try_emplace(notificationName, std::move(newSnapshotPtr));
+      }
+    }
+  }
+}
+
+IReactNotificationSubscription ReactNotificationService::Subscribe(
+    IReactDispatcher const &dispatcher,
+    IReactPropertyName const &notificationName,
+    ReactNotificationHandler const &handler) noexcept {
+  auto subscription = make<ReactNotificationSubscription>(get_weak(), dispatcher, notificationName, handler);
+  ModifySubscriptions(
+      notificationName, [&subscription](std::vector<IReactNotificationSubscription> const &snapshot) noexcept {
+        std::vector<IReactNotificationSubscription> newSnapshot(snapshot);
+        newSnapshot.push_back(subscription);
+        return newSnapshot;
+      });
+  return subscription;
+}
+
+void ReactNotificationService::Unsubscribe(IReactNotificationSubscription const &subscription) noexcept {
+  ModifySubscriptions(
+      subscription.NotificationName(),
+      [&subscription](std::vector<IReactNotificationSubscription> const &snapshot) noexcept {
+        std::vector<IReactNotificationSubscription> newSnapshot(snapshot);
+        auto it = std::find(newSnapshot.begin(), newSnapshot.end(), subscription);
+        if (it != newSnapshot.end()) {
+          newSnapshot.erase(it);
+        }
+        return newSnapshot;
+      });
+}
+
+void ReactNotificationService::SendNotification(
+    IReactPropertyName const &notificationName,
+    IInspectable const &sender,
+    IInspectable const &data) noexcept {
+  SubscriptionSnapshotPtr currentSnapshotPtr;
+
+  {
+    std::scoped_lock readLock{m_mutex};
+    currentSnapshotPtr = m_subscriptions[notificationName];
+  }
+
+  // Call notification handlers outside of lock.
+  if (currentSnapshotPtr) {
+    for (auto &subscription : *currentSnapshotPtr) {
+      auto notificationData = make<ReactNotificationData>(subscription, data);
+      get_self<ReactNotificationSubscription>(subscription)->CallHandler(sender, notificationData);
+    }
+  }
+
+  // Call parent notification service
+  if (m_parentNotificationService) {
+    m_parentNotificationService.SendNotification(notificationName, sender, data);
+  }
+}
+
+//=============================================================================
+// ReactNotificationServiceProxy implementation
+//=============================================================================
+
+ReactNotificationServiceProxy::ReactNotificationServiceProxy(weak_ref<IReactNotificationService> &&service) noexcept
+    : m_service{std::move(service)} {}
+
+ReactNotificationServiceProxy::~ReactNotificationServiceProxy() = default;
+
+IReactNotificationSubscription ReactNotificationServiceProxy::Subscribe(
+    IReactDispatcher const &dispatcher,
+    IReactPropertyName const &notificationName,
+    ReactNotificationHandler const &handler) noexcept {
+  if (auto service = m_service.get()) {
+    return service.Subscribe(dispatcher, notificationName, handler);
+  } else {
+    return nullptr;
+  }
+}
+
+void ReactNotificationServiceProxy::SendNotification(
+    IReactPropertyName const &notificationName,
+    IInspectable const &sender,
+    IInspectable const &data) noexcept {
+  if (auto service = m_service.get()) {
+    service.SendNotification(notificationName, sender, data);
+  }
+}
+
+//=============================================================================
+// ReactNotificationServiceHelper implementation
+//=============================================================================
+
+Microsoft::ReactNative::IReactNotificationService ReactNotificationServiceHelper::CreateNotificationService() noexcept {
+  return make<ReactNotificationService>();
+}
+
 } // namespace winrt::Microsoft::ReactNative::implementation
--- vnext/Microsoft.ReactNative/IReactNotificationService.h
+++ vnext/Microsoft.ReactNative/IReactNotificationService.h
@@ -3,17 +3,117 @@
 
 #pragma once
 #include "ReactNotificationServiceHelper.g.h"
+#include <functional/functorRef.h>
+#include <object/objectRefCount.h>
+#include <winrt/Microsoft.ReactNative.h>
+#include <atomic>
+#include <map>
+#include <mutex>
+#include <vector>
 
 namespace winrt::Microsoft::ReactNative::implementation {
+
+struct ReactNotificationData : implements<ReactNotificationData, IReactNotificationData> {
+  ReactNotificationData(IReactNotificationSubscription const &subscription, IInspectable const &data) noexcept
+      : m_subscription{subscription}, m_data{data} {}
+
+  IReactNotificationSubscription Subscription() const noexcept {
+    return m_subscription;
+  }
+
+  IInspectable Data() const noexcept {
+    return m_data;
+  }
+
+ private:
+  IReactNotificationSubscription m_subscription;
+  IInspectable m_data;
+};
+
+struct ReactNotificationService : implements<ReactNotificationService, IReactNotificationService> {
+  ReactNotificationService();
+  explicit ReactNotificationService(IReactNotificationService const parentNotificationService) noexcept;
+  ~ReactNotificationService() noexcept;
+
+  IReactNotificationSubscription Subscribe(
+      IReactDispatcher const &dispatcher,
+      IReactPropertyName const &notificationName,
+      ReactNotificationHandler const &handler) noexcept;
+
+  void Unsubscribe(IReactNotificationSubscription const &subscription) noexcept;
+
+  void SendNotification(
+      IReactPropertyName const &notificationName,
+      IInspectable const &sender,
+      IInspectable const &data) noexcept;
+
+ private:
+  // We treat subscription snapshots as immutable data.
+  using SubscriptionSnapshot = std::vector<IReactNotificationSubscription>;
+  using SubscriptionSnapshotPtr = Mso::RefCountedPtr<SubscriptionSnapshot>;
+
+  void ModifySubscriptions(
+      IReactPropertyName const &notificationName,
+      Mso::FunctorRef<SubscriptionSnapshot(SubscriptionSnapshot const &)> const &modifySnapshot);
+
+ private:
+  std::mutex m_mutex;
+  std::map<IReactPropertyName, SubscriptionSnapshotPtr> m_subscriptions;
+  IReactNotificationService m_parentNotificationService;
+};
+
+struct ReactNotificationServiceProxy : implements<ReactNotificationServiceProxy, IReactNotificationService> {
+  ReactNotificationServiceProxy(weak_ref<IReactNotificationService> &&service) noexcept;
+  ~ReactNotificationServiceProxy() noexcept;
+
+  IReactNotificationSubscription Subscribe(
+      IReactDispatcher const &dispatcher,
+      IReactPropertyName const &notificationName,
+      ReactNotificationHandler const &handler) noexcept;
+
+  void SendNotification(
+      IReactPropertyName const &notificationName,
+      IInspectable const &sender,
+      IInspectable const &data) noexcept;
+
+ private:
+  weak_ref<IReactNotificationService> m_service;
+};
+
 struct ReactNotificationServiceHelper {
   ReactNotificationServiceHelper() = default;
 
-  static Microsoft::ReactNative::IReactNotificationService CreateNotificationService();
+  static IReactNotificationService CreateNotificationService() noexcept;
 };
+
+struct ReactNotificationSubscription : implements<ReactNotificationSubscription, IReactNotificationSubscription> {
+  ReactNotificationSubscription(
+      weak_ref<ReactNotificationService> &&notificationService,
+      IReactDispatcher const &dispatcher,
+      IReactPropertyName const &notificationName,
+      ReactNotificationHandler const &handler) noexcept;
+  ~ReactNotificationSubscription() noexcept;
+
+  IReactDispatcher Dispatcher() const noexcept;
+  IReactPropertyName NotificationName() const noexcept;
+  bool IsSubscribed() const noexcept;
+  void Unsubscribe() noexcept;
+  void CallHandler(IInspectable const &sender, IReactNotificationData const &notificationData) noexcept;
+
+ private:
+  weak_ref<ReactNotificationService> m_notificationService;
+  IReactDispatcher m_dispatcher;
+  IReactPropertyName m_notificationName;
+  ReactNotificationHandler m_handler;
+  std::atomic_bool m_isSubscribed{true};
+};
+
 } // namespace winrt::Microsoft::ReactNative::implementation
 
 namespace winrt::Microsoft::ReactNative::factory_implementation {
+
 struct ReactNotificationServiceHelper
     : ReactNotificationServiceHelperT<ReactNotificationServiceHelper, implementation::ReactNotificationServiceHelper> {
 };
+
 } // namespace winrt::Microsoft::ReactNative::factory_implementation
