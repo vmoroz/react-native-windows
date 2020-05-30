d59ce0369b749168ab409d3dc10768e2fa30afb6
Vladimir Morozov <vmorozov@microsoft.com>
2020-05-30 11:17:18 -0700

    Refactored UI.Xaml.impl.override.h

--- vnext/Microsoft.ReactNative.Cxx/UI.Xaml.impl.override.h
+++ vnext/Microsoft.ReactNative.Cxx/UI.Xaml.impl.override.h
@@ -1,37 +1,38 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+//
+// In this file we override some of the generated code to consume XAML ABI.
 #pragma once
 
-//
-// In this file we override some of the XAML ABI consume code.
-//
-
 #include <winrt/base.h>
-
 #ifndef USE_WINUI3
 #include <winrt/impl/Windows.UI.Xaml.2.h>
-#define XAML_NS Windows::UI::Xaml
-#define consume_XAML_IApplicationStatics consume_Windows_UI_Xaml_IApplicationStatics
 #else
 #include <winrt/impl/Microsoft.UI.Xaml.2.h>
-#define XAML_NS Microsoft::UI::Xaml
-#define consume_XAML_IApplicationStatics consume_Microsoft_UI_Xaml_IApplicationStatics
 #endif
 
 namespace winrt::impl {
 
-// Override xaml::Application::Currnet() to return nullptr instead of crashing
+// Override xaml::Application::Current() to return nullptr instead of crashing
 // if Application is not found.
+#ifndef USE_WINUI3
 template <>
-WINRT_IMPL_AUTO(XAML_NS::Application)
-inline consume_XAML_IApplicationStatics<XAML_NS::IApplicationStatics>::Current() const {
-  using D = XAML_NS::IApplicationStatics;
+WINRT_IMPL_AUTO(Windows::UI::Xaml::Application)
+inline consume_Windows_UI_Xaml_IApplicationStatics<Windows::UI::Xaml::IApplicationStatics>::Current() const {
+  using D = Windows::UI::Xaml::IApplicationStatics; // for the WINRT_IMPL_SHIM macro
   void *value{};
-  WINRT_IMPL_SHIM(XAML_NS::IApplicationStatics)->get_Current(&value);
-  return XAML_NS::Application{value, take_ownership_from_abi};
+  WINRT_IMPL_SHIM(Windows::UI::Xaml::IApplicationStatics)->get_Current(&value);
+  return Windows::UI::Xaml::Application{value, take_ownership_from_abi};
 }
+#else
+template <>
+WINRT_IMPL_AUTO(Microsoft::UI::Xaml::Application)
+inline consume_Microsoft_UI_Xaml_IApplicationStatics<Microsoft::UI::Xaml::IApplicationStatics>::Current() const {
+  using D = Microsoft::UI::Xaml::IApplicationStatics; // for the WINRT_IMPL_SHIM macro
+  void *value{};
+  WINRT_IMPL_SHIM(Microsoft::UI::Xaml::IApplicationStatics)->get_Current(&value);
+  return Microsoft::UI::Xaml::Application{value, take_ownership_from_abi};
+}
+#endif
 
 } // namespace winrt::impl
-
-#undef XAML_NS
-#undef consume_XAML_IApplicationStatics
