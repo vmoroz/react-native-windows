33679a8ee7bfa823718f3f7f413ca744f924a968
Vladimir Morozov <vmorozov@microsoft.com>
2020-07-15 18:35:09 -0700

    Add JSI unit tests

---
+++ vnext/Microsoft.ReactNative.IntegrationTests/JsiRuntimeTests.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "jsi/test/testlib.h"
+
+namespace facebook::jsi {
+
+std::vector<RuntimeFactory> runtimeGenerators() {
+  return {};
+}
+
+} // namespace facebook::jsi
