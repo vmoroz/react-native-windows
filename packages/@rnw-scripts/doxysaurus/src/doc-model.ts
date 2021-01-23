bddb46faf0c95cb83b13c8738a294ef2a384bfc7
Vladimir Morozov <vmorozov@microsoft.com>
2021-01-23 06:14:29 -0800

    Add new members to doc-model

--- packages/@rnw-scripts/doxysaurus/src/doc-model.ts
+++ packages/@rnw-scripts/doxysaurus/src/doc-model.ts
@@ -13,4 +13,26 @@ export class DocCompound {
   namespace?: string;
   typeName?: string;
   docId?: string;
+
+  brief?: string;
+  details?: string;
+
+  sections = new Map<string, DocSection>();
+  // TODO: add templateParams
+
+  baseCompounds: DocCompound[] = [];
 }
+
+export class DocSection {
+  memberOverloads = new Map<string, DocMemberOverload>();
+}
+
+export class DocMemberOverload {
+  members = new Map<string, DocMember>();
+}
+
+export class DocMember {
+  parameters = new Map<string, DocParameter>();
+}
+
+export class DocParameter {}
