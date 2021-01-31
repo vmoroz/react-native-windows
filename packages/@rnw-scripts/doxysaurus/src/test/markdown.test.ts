c3fc3b12fc349db5e7f67a6d7eeaa64ca4161f32
Vladimir Morozov <vmorozov@microsoft.com>
2021-01-30 19:54:19 -0800

    Add two simple tests for toMarkdown

--- packages/@rnw-scripts/doxysaurus/src/test/markdown.test.ts
+++ packages/@rnw-scripts/doxysaurus/src/test/markdown.test.ts
@@ -5,6 +5,43 @@
  * @format
  */
 
-test('Dummy test', () => {
-  expect('aaa').toBe('aaa');
+import * as xml2js from 'xml2js';
+import {applyTemplateRules} from './string-template';
+import {DoxMember} from '../doxygen-model';
+import {toMarkdown} from '../markdown';
+
+test('Empty brief description', async () => {
+  const memberDef = await parse(`
+    |<memberdef>
+    |  <briefdescription>
+    |  </briefdescription>
+    |</memberdef>`);
+
+  const text = toMarkdown(memberDef.briefdescription);
+  expect(text).toBe('');
 });
+
+test('Brief description with "para" tag', async () => {
+  const memberDef = await parse(`
+    |<memberdef>
+    |  <briefdescription>
+    |    <para>Test </para>
+    |  </briefdescription>
+    |</memberdef>`);
+
+  const text = toMarkdown(memberDef.briefdescription);
+  expect(text).toBe('Test \n\n');
+});
+
+async function parse(xmlText: string) {
+  const xml = await xml2js.parseStringPromise(t(xmlText), {
+    explicitChildren: true,
+    preserveChildrenOrder: true,
+    charsAsChildren: true,
+  });
+  return xml.memberdef as DoxMember;
+}
+
+function t(text: string) {
+  return applyTemplateRules(text, {indent: '  ', EOL: '\n'}).trim();
+}
