73a7ec95be1e55b741019bb7854eedecbaa203bc
Vladimir Morozov <vmorozov@microsoft.com>
2021-02-11 09:41:09 -0800

    Improve compilation speed

--- vnext/Microsoft.ReactNative/Pch/pch.h
+++ vnext/Microsoft.ReactNative/Pch/pch.h
@@ -26,8 +26,38 @@
 // When WINAPI_FAMILY is DESKTOP_APP, windows.h creates a macro for GetCurrentTime, which conflicts with other headers
 #undef GetCurrentTime
 
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.Devices.Input.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Metadata.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.Networking.Sockets.h>
+#include <winrt/Windows.Security.Cryptography.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.ViewManagement.Core.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#include <winrt/Windows.UI.Xaml.Automation.Peers.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Input.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <winrt/Windows.UI.Xaml.Shapes.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.Web.Http.Filters.h>
+
+#include <ReactCommon/TurboModule.h>
+
+#include <jsrt.h>
+
+#include "Base/CxxReactIncludes.h"
 #include "CppWinRTIncludes.h"
 #include "HResult.h"
+
+#include <cxxreact/MessageQueueThread.h>
+
+#include <boost/algorithm/string.hpp>
 
 #include <algorithm>
 #include <atomic>
@@ -43,6 +73,7 @@
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <regex>
 #include <set>
 #include <sstream>
 #include <string>
@@ -50,4 +81,6 @@
 #include <vector>
 
 #include <activeObject/activeObject.h>
+#include <functional/functorRef.h>
 #include <future/future.h>
+
