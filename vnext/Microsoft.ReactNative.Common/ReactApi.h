0997005e1976233653488957f451348a05f621f0
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-11-01 14:43:31 -0700

    Add more API

--- vnext/Microsoft.ReactNative.Common/ReactApi.h
+++ vnext/Microsoft.ReactNative.Common/ReactApi.h
@@ -63,6 +63,8 @@ typedef enum {
   react_property_type_rect_array = react_property_type_rect + 1024
 } react_property_type;
 
+typedef enum { react_js_engine_chakra = 0, react_js_engine_hermes = 1, react_js_engine_v8 = 2 } react_js_engine;
+
 typedef struct react_object_t *react_object;
 typedef struct react_property_namespace_t *react_property_namespace;
 typedef struct react_property_name_t *react_property_name;
@@ -72,6 +74,12 @@ typedef struct react_string_t *react_string;
 typedef struct react_dispatcher_t *react_dispatcher;
 typedef struct react_notification_service_t *react_notification_service;
 typedef struct react_notification_subscription_t *react_notification_subscription;
+typedef struct react_host_t *react_host;
+typedef struct react_host_builder_t *react_host_builder;
+typedef struct react_extension_package_t *react_extension_package;
+typedef struct react_instance_builder_t *react_instance_builder;
+typedef struct react_red_box_handler_t *react_red_box_handler;
+typedef struct react_log_handler_t *react_log_handler;
 
 typedef struct {
   int64_t value;
@@ -347,6 +355,271 @@ react_status REACT_STDCALL react_notification_service_release(react_notification
 react_status REACT_STDCALL
 react_notification_service_to_object(react_notification_service service, react_object *result);
 react_status REACT_STDCALL react_notification_service_from_object(react_object obj, react_notification_service *result);
+
+react_status REACT_STDCALL react_host_builder_create(react_host_builder *result);
+react_status REACT_STDCALL react_host_builder_create_copy(react_host_builder builder, react_host_builder *result);
+react_status REACT_STDCALL
+react_host_builder_add_extension_package(react_host_builder builder, react_extension_package package);
+
+react_status REACT_STDCALL react_host_builder_add_ref(react_host_builder builder);
+react_status REACT_STDCALL react_host_builder_release(react_host_builder builder);
+react_status REACT_STDCALL react_host_builder_to_object(react_host_builder builder, react_object *result);
+react_status REACT_STDCALL react_host_builder_from_object(react_object obj, react_host_builder *result);
+
+//  DOC_STRING("Used in @LogHandler to represent different LogLevels")
+// enum LogLevel { Trace = 0, Info = 1, Warning = 2, Error = 3, Fatal = 4 };
+//
+// DOC_STRING("delegate to represent a logging function.")
+// delegate void LogHandler(LogLevel level, String message);
+//
+// DOC_STRING("A JavaScript engine type that can be set for a React instance in
+// @ReactInstanceSettings.JSIEngineOverride")
+//// Keep in sync with enum in IReactInstance.h
+// enum JSIEngine { Chakra = 0, Hermes = 1, V8 = 2 };
+//
+//[webhosthidden][default_interface] DOC_STRING(
+//     "The arguments for the @ReactInstanceSettings.InstanceCreated event.") runtimeclass InstanceCreatedEventArgs {
+//   DOC_STRING("Gets the @IReactContext for the React instance that was just created.")
+//   IReactContext Context {
+//     get;
+//   };
+// }
+//
+//     [webhosthidden][default_interface] DOC_STRING("The arguments for the @ReactInstanceSettings.InstanceLoaded
+//     event.")
+//         runtimeclass InstanceLoadedEventArgs {
+//   DOC_STRING("Gets the @IReactContext for the React instance that finished loading the bundle.")
+//   IReactContext Context {
+//     get;
+//   };
+//
+//   DOC_STRING("Returns `true` if the JavaScript bundle failed to load.")
+//   Boolean Failed {
+//     get;
+//   };
+// }
+//
+//[webhosthidden][default_interface] DOC_STRING("The arguments for the @ReactInstanceSettings.InstanceDestroyed event.")
+//     runtimeclass InstanceDestroyedEventArgs {
+//       DOC_STRING("Gets the @IReactContext for the React instance that just destroyed.")
+//       IReactContext Context {
+//         get;
+//       };
+//     }
+//
+//         [webhosthidden] DOC_STRING("Provides settings to create a React instance.") runtimeclass
+//         ReactInstanceSettings {
+//   DOC_STRING("Creates new instance of @ReactInstanceSettings")
+//   ReactInstanceSettings();
+//
+//   DOC_STRING(
+//       "Gets a @IReactPropertyBag to share values between components and the application.\n"
+//       "Use @IReactContext.Properties to access this @IReactPropertyBag from native components and view managers.")
+//   IReactPropertyBag Properties {
+//     get;
+//   };
+//
+//   DOC_STRING(
+//       "Gets a @IReactNotificationService to send notifications between components and the application.\n"
+//       "Use @IReactContext.Notifications to access this @IReactNotificationService "
+//       "from native components or view managers.")
+//   IReactNotificationService Notifications {
+//     get;
+//   };
+//
+//   DOC_STRING(
+//       "Gets a list of @IReactPackageProvider.\n"
+//       "Add an implementation of @IReactPackageProvider to this list to define additional "
+//       "native modules and custom view managers to be included in the React instance.\n"
+//       "Auto-linking automatically adds @IReactPackageProvider to the application's @.PackageProviders.")
+//   IVector<IReactPackageProvider> PackageProviders {
+//     get;
+//   };
+//
+//   DOC_STRING(
+//       "This controls whether various developer experience features are available for this instance. "
+//       "In particular, it enables the developer menu, the default `RedBox` and `LogBox` experience.")
+//   Boolean UseDeveloperSupport {
+//     get;
+//     set;
+//   };
+//
+//   DOC_STRING(
+//       "The name of the JavaScript bundle file to load. This should be a relative path from @.BundleRootPath. "
+//       "The `.bundle` extension will be appended to the end, when looking for the bundle file.\n"
+//       "If using an embedded RCDATA resource, this identifies the resource ID that stores the bundle. See
+//       @.BundleRootPath.")
+//   DOC_DEFAULT("index.windows")
+//   String JavaScriptBundleFile {
+//     get;
+//     set;
+//   };
+//
+//   DOC_STRING(
+//       "Controls whether the instance JavaScript runs in a remote environment such as within a browser.\n"
+//       "By default, this is using a browser navigated to http://localhost:8081/debugger-ui served by Metro/Haul.\n"
+//       "Debugging will start as soon as the react native instance is loaded.")
+//   Boolean UseWebDebugger {
+//     get;
+//     set;
+//   };
+//
+//   DOC_STRING(
+//       "Controls whether the instance triggers the hot module reload logic when it first loads the instance.\n"
+//       "Most edits should be visible within a second or two without the instance having to reload.\n"
+//       "Non-compatible changes still cause full reloads.\n"
+//       "See [Fast Refresh](https://reactnative.dev/docs/fast-refresh) for more information on Fast Refresh.")
+//   Boolean UseFastRefresh {
+//     get;
+//     set;
+//   };
+//
+
+typedef enum {
+  react_instance_use_direct_debugging = 1 << 0,
+  react_instance_debugger_break_on_start = 1 << 1,
+  react_instance_enable_jit_compilation = 1 << 2,
+  react_instance_enable_bytecode_caching = 1 << 3,
+  react_instance_enable_default_crash_handler = 1 << 4,
+  react_instance_enable_request_inline_source_map = 1 << 5,
+} react_instance_flags;
+
+react_status REACT_STDCALL
+react_instance_builder_set_flags(react_instance_builder instance, react_instance_flags flags);
+react_status REACT_STDCALL
+react_instance_builder_get_flags(react_instance_builder instance, react_instance_flags *result);
+
+react_status REACT_STDCALL
+react_instance_builder_set_byte_code_file_uri(react_instance_builder instance, const char *value, size_t value_length);
+
+react_status REACT_STDCALL
+react_instance_builder_set_debug_bundle_path(react_instance_builder instance, const char *value, size_t value_length);
+
+react_status REACT_STDCALL react_instance_builder_set_bundle_root_path(
+    react_instance_builder instance, const char *value, size_t value_length);
+
+react_status REACT_STDCALL react_instance_builder_set_debugger_port(
+    react_instance_builder instance,
+    uint16_t value);
+
+react_status REACT_STDCALL
+react_instance_builder_set_debugger_runtime_name(react_instance_builder instance, const char *value, size_t value_length);
+
+react_status REACT_STDCALL react_instance_builder_set_red_box_handler(
+    react_instance_builder instance, react_red_box_handler handler);
+
+react_status REACT_STDCALL
+react_instance_builder_set_native_logger(react_instance_builder instance, react_log_handler handler);
+
+react_status REACT_STDCALL
+react_instance_builder_set_ui_dispatcher(react_instance_builder instance, react_dispatcher dispatcher);
+
+react_status REACT_STDCALL
+react_instance_builder_set_source_bundle_host(react_instance_builder instance, const char *value, size_t value_length);
+
+react_status REACT_STDCALL
+react_instance_builder_set_source_bundle_port(react_instance_builder instance, uint16_t port);
+
+react_status REACT_STDCALL
+react_instance_builder_set_js_engine(react_instance_builder instance, react_js_engine engine);
+
+
+//   DOC_STRING(
+//       "The @InstanceCreated event is triggered right after the React Native instance is created.\n"
+//       "\n"
+//       "It is triggered on the JSDispatcher thread before any other JSDispatcher work items.\n"
+//       "No JavaScript code is loaded in the JavaScript engine yet.\n"
+//       "The @InstanceCreatedEventArgs.Context property on the event arguments provides access to the instance
+//       context.\n"
+//       "\n"
+//       "Note that the @InstanceCreated event is triggered in response to the 'InstanceCreated' notification "
+//       "raised in the 'ReactNative.InstanceSettings' namespace. "
+//       "Consider using @Notifications to handle the notification in a dispatcher different from the JSDispatcher.")
+//   event Windows.Foundation.EventHandler<InstanceCreatedEventArgs> InstanceCreated;
+//
+//   DOC_STRING(
+//       "The @InstanceLoaded event is triggered when React Native instance has finished loading the JavaScript
+//       bundle.\n"
+//       "\n"
+//       "It is triggered on the JSDispatcher thread.\n"
+//       "If there were errors, then the @InstanceLoadedEventArgs.Failed property on the event arguments will be
+//       true.\n" "The error types include:\n"
+//       "\n"
+//       "* JavaScript syntax errors.\n"
+//       "* Global JavaScript exceptions thrown.\n"
+//       "\n"
+//       "The @InstanceLoadedEventArgs.Context property on the event arguments provides access to the instance
+//       context.\n"
+//       "\n"
+//       "Note that the @InstanceLoaded event is triggered in response to the 'InstanceLoaded' notification "
+//       "raised in the 'ReactNative.InstanceSettings' namespace. "
+//       "Consider using @Notifications to handle the notification in a dispatcher different from the JSDispatcher.")
+//   event Windows.Foundation.EventHandler<InstanceLoadedEventArgs> InstanceLoaded;
+//
+//   DOC_STRING(
+//       "The @InstanceDestroyed event is triggered when React Native instance is destroyed.\n"
+//       "\n"
+//       "It is triggered on the JSDispatcher thread as the last work item before it shuts down.\n"
+//       "No new JSDispatcher work can be executed after that.\n"
+//       "The @InstanceDestroyedEventArgs.Context property on the event arguments provides access to the instance
+//       context.\n"
+//       "\n"
+//       "Note that the @InstanceDestroyed event is triggered in response to the 'InstanceDestroyed' notification "
+//       "raised in the 'ReactNative.InstanceSettings' namespace. "
+//       "Consider using @Notifications to handle the notification in a dispatcher different from the JSDispatcher.")
+//   event Windows.Foundation.EventHandler<InstanceDestroyedEventArgs> InstanceDestroyed;
+// }
+
+react_status REACT_STDCALL react_host_add_ref(react_host host);
+react_status REACT_STDCALL react_host_release(react_host host);
+react_status REACT_STDCALL react_host_to_object(react_host host, react_object *result);
+react_status REACT_STDCALL react_host_from_object(react_object obj, react_host *result);
+
+//    runtimeclass ReactNativeHost {
+//      DOC_STRING("Creates a new instance of @ReactNativeHost")
+//      ReactNativeHost();
+//
+//      DOC_STRING(
+//          "Provides access to the list of @IReactPackageProvider's that the React instance will use to provide "
+//          "native modules to the application. This can be used to register additional package providers, such as "
+//          "package providers from community modules or other shared libraries.")
+//      IVector<IReactPackageProvider> PackageProviders {
+//        get;
+//      };
+//
+//      DOC_STRING("Provides access to this host's @ReactInstanceSettings to configure the react instance.")
+//      ReactInstanceSettings InstanceSettings {
+//        get;
+//        set;
+//      };
+//
+//      DOC_STRING("Loads a new React instance. It is an alias for @.ReloadInstance method.")
+//      Windows.Foundation.IAsyncAction LoadInstance();
+//
+//      DOC_STRING(
+//          "Unloads the current React instance and loads a new one.\n"
+//          "The React instance loading creates an instance of the JavaScript engine, "
+//          "and launches the provided JavaScript code bundle.\n"
+//          "If a React instance is already running in this host, then @.ReloadInstance shuts down the already "
+//          "the running React instance, and loads a new React instance.\n"
+//          "The React instance lifecycle can be observed with the following events:"
+//          "- The @ReactInstanceSettings.InstanceCreated event is raised when the React instance is just created.\n"
+//          "- The @ReactInstanceSettings.InstanceLoaded event is raised when the React instance completed"
+//          "  loading the JavaScript bundle.\n"
+//          "- The @ReactInstanceSettings.InstanceDestroyed event is raised when the React instance is destroyed.")
+//      Windows.Foundation.IAsyncAction ReloadInstance();
+//
+//      DOC_STRING(
+//          "Unloads current React instance.\n"
+//          "After the React instance is unloaded, all the React resources including the JavaScript engine environment "
+//          "are cleaned up.\n"
+//          "The React instance destruction can be observed with the @ReactInstanceSettings.InstanceDestroyed event.")
+//      Windows.Foundation.IAsyncAction UnloadInstance();
+//
+//      DOC_STRING("Returns the @ReactNativeHost instance associated with the given @IReactContext.")
+//      static ReactNativeHost FromContext(IReactContext reactContext);
+//    }
+//} // namespace ReactNative
 
 REACT_EXTERN_C_END
 
