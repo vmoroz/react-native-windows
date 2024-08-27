f884c6b8f7fcc205f06fabcd171a00f239aad929
Vladimir Morozov <vmorozov@microsoft.com>
2024-08-27 16:57:10 -0700

    Apply Fusebox related changes from erozell

--- vnext/Shared/OInstance.cpp
+++ vnext/Shared/OInstance.cpp
@@ -46,6 +46,8 @@
 #include <WebSocketJSExecutorFactory.h>
 #include <safeint.h>
 #include "PackagerConnection.h"
+#include "Threading/MessageDispatchQueue.h"
+#include "FuseboxInspectorThread.h"
 
 #if defined(USE_HERMES) && defined(ENABLE_DEVSERVER_HBCBUNDLES)
 #include <hermes/BytecodeVersion.h>
@@ -329,17 +331,17 @@ void InstanceImpl::SetInError() noexcept {
 }
 
 namespace {
-bool shouldStartHermesInspector(DevSettings &devSettings) {
-  bool isHermes =
+  bool shouldStartHermesInspector(DevSettings& devSettings) {
+    bool isHermes =
       ((devSettings.jsiEngineOverride == JSIEngineOverride::Hermes) ||
-       (devSettings.jsiEngineOverride == JSIEngineOverride::Default && devSettings.jsiRuntimeHolder &&
-        devSettings.jsiRuntimeHolder->getRuntimeType() == facebook::react::JSIEngineOverride::Hermes));
+        (devSettings.jsiEngineOverride == JSIEngineOverride::Default && devSettings.jsiRuntimeHolder &&
+          devSettings.jsiRuntimeHolder->getRuntimeType() == facebook::react::JSIEngineOverride::Hermes));
 
-  if (isHermes && devSettings.useDirectDebugger && !devSettings.useWebDebugger)
-    return true;
-  else
-    return false;
-}
+    if (isHermes && devSettings.useDirectDebugger && !devSettings.useWebDebugger)
+      return true;
+    else
+      return false;
+  }
 } // namespace
 
 InstanceImpl::InstanceImpl(
@@ -373,7 +375,8 @@ InstanceImpl::InstanceImpl(
 #endif
 
   if (shouldStartHermesInspector(*m_devSettings)) {
-    m_devManager->EnsureHermesInspector(m_devSettings->sourceBundleHost, m_devSettings->sourceBundlePort);
+    devManager->EnsureHermesInspector(
+      devSettings->sourceBundleHost, devSettings->sourceBundlePort, devSettings->bundleAppId);
   }
 
   // Default (common) NativeModules
@@ -483,7 +486,8 @@ InstanceImpl::InstanceImpl(
     }
   }
 
-  m_innerInstance->initializeBridge(std::move(callback), jsef, m_jsThread, m_moduleRegistry);
+  m_innerInstance->initializeBridge(
+      std::move(callback), jsef, m_jsThread, m_moduleRegistry, m_devSettings->inspectorTarget);
 
   // For RuntimeScheduler to work properly, we need to install TurboModuleManager with RuntimeSchedulerCallbackInvoker.
   // To be able to do that, we need to be able to call m_innerInstance->getRuntimeExecutor(), which we can only do after
@@ -586,6 +590,16 @@ void InstanceImpl::loadBundleInternal(std::string &&jsBundleRelativePath, bool s
 }
 
 InstanceImpl::~InstanceImpl() {
+  if (m_devSettings->inspectorTarget) {
+    auto messageDispatchQueue =
+        Mso::React::MessageDispatchQueue(::Microsoft::ReactNative::FuseboxInspectorThread::Instance(), nullptr);
+    messageDispatchQueue.runOnQueueSync([weakInnerInstance = std::weak_ptr(m_innerInstance)]() {
+      if (auto innerInstance = weakInnerInstance.lock()) {
+        innerInstance->unregisterFromInspector();
+      }
+    });
+  }
+
   if (shouldStartHermesInspector(*m_devSettings) && m_devSettings->jsiRuntimeHolder) {
     m_devSettings->jsiRuntimeHolder->teardown();
   }
